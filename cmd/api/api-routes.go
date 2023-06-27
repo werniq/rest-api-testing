61c891a1e1b9e327cb50f3d2ce1bf404e12af151
Oleksandr <qniwwwersss@gmail.com>
2023-06-27 09:32:55 +0200

    Testing REST API with Golang

---
+++ cmd/api/api-routes.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(app.enableCORS)
+
+	// authentication routes - auth handler, refresh
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
+
+	// protected routes
+	mux.Route("/users", func(mux chi.Router) {
+		mux.Use(app.authRequired)
+
+		mux.Get("/", app.allUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/", app.insertUser)
+		mux.Patch("/", app.updateUser)
+	})
+
+
+	return mux
+}
